9678da88c8e1b7252d4708c5c07256e78a8c3295
Mihir Singh Chouhan <156662154+Mihir-Singh-Chouhan@users.noreply.github.com>
2026-07-17 17:02:17 +0530

    Delete Java 8 Features directory

--- Java 8 Features/src/Functional_Interface/Main.java
+++
@@ -1,9 +0,0 @@
-package Functional_Interface;
-
-public class Main {
-    public static void main(String[] arg){
-        System.out.println("Functional Interface Notes : \n");
-        Defination define = new Defination();
-        define.getFunctionalInterface();
-    }
-}
